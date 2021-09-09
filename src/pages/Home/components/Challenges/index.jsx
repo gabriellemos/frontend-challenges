0ed0691a08317208b4441e68f7c2938405fb23b9
Gabriel Lemos <gabrieldebritolemos@gmail.com>
2021-09-09 00:28:00 -0300

    Updated 'Home' layout

--- src/pages/Home/components/Challenges/index.jsx
+++ src/pages/Home/components/Challenges/index.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 
 import ChallengeCard from '../ChallengeCard'
+import * as Styled from './styled'
 
 const Challenges = () => {
   return (
-    <React.Fragment>
+    <Styled.Container>
       <ChallengeCard
         title="Order summary component"
         description="A perfect project for newbies who are starting to build confidence with layouts!"
         image="https://res.cloudinary.com/dz209s6jk/image/upload/q_auto,w_700/Challenges/ouobx6tvyduidzio7wr7.jpg"
         redirectTo="/order-summary"
       />
-    </React.Fragment>
+    </Styled.Container>
   )
 }
 
--- src/pages/Home/styled.jsx
+++ src/pages/Home/styled.jsx
@@ -1,9 +1,9 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  width: 100vw;
-  height: 100vh;
+  max-width: 100%;
+  min-height: 100vh;
   display: flex;
-  align-items: center;
+  align-items: flex-start;
   justify-content: center;
 `
